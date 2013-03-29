f8ed6d57aabafb763be13cf0491fe188253f9f5d
Kevin_Xi <kevin.xgr@gmail.com>
2013-03-29 23:47:27 +0800

    static file, templates settings and index

--- qiubangmang/urls.py
+++ qiubangmang/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from views import index
+import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,4 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+	('^$', index),
+	(r'^site_medias/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATICFILES_DIRS,'show_indexes':True}),
 )
